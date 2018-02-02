ad52a61016178ba108430c07d7971d60f6f2dc74
ryanblanchard237 <mr.ryan.blanchard@gmail.com>
2018-02-02 02:10:51 -0800

    Now the program prints a third line as well.

--- some_source.cpp
+++ some_source.cpp
@@ -5,6 +5,7 @@ int main(void)
 {
     printf("Here's some program.\n");
     printf("All it does is print these two lines to the screen.\n");
+    printf("Just kidding; now it prints a third line as well.\n");
     
     return 0;
 }
